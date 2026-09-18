5736ddb621e236090888a310ccc4ab24093faed7
messem254-alt <messem254@gmail.com>
2026-09-18 07:48:50 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
 
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Learner(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
 class Course(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    instructor = models.ForeignKey(
+        Instructor,
+        on_delete=models.CASCADE,
+        related_name="courses"
+    )
 
     def __str__(self):
         return self.name
